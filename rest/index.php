e5a9c45f4bc72e463515538210e7ea4c77237915
Aldin Talic <aldintalic@Aldin-MacBook.local>
2023-07-08 18:13:13 +0200

    commit

--- rest/index.php
+++ rest/index.php
@@ -12,18 +12,7 @@ require 'routes/MidtermRoutes.php';
 require 'routes/FinalRoutes.php';
 
 // Middleware to protect routes
-Flight::before('start', function () {
-    $protectedRoutes = ['/rest/final/share_classes', '/rest/final/share_class_categories'];
-    $token = $_SERVER['HTTP_AUTHORIZATION'] ?? '';
 
-    // Verify token
-    try {
-        $decoded = JWT::decode($token, Key::getKey(), ['HS256']);
-        // Proceed with the request
-    } catch (Exception $e) {
-        Flight::halt(401, 'Unauthorized');
-    }
-});
 
 Flight::start();
 ?>
